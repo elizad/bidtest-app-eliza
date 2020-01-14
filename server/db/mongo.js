6994a15d3de7b3a24f7e739ff5a9d060f7c5e8b9
elizad <elizadascal@gmail.com>
2020-01-14 01:21:28 +0000

    updating server part

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 import Buyers from "../models/buyers";
 import Bids from "../models/bids";
 import Sellers from "../models/sellers";
-import Competition from "../models/competition";
+import Competition from '../models/competition';
 
 const connectDb = () => {
-  return mongoose.connect("mongodb://localhost:27017");
+  return mongoose.connect('mongodb://localhost:27017');
 };
 
 const models = { Buyers, Bids, Sellers, Competition };
