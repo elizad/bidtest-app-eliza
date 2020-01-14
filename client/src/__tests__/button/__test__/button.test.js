7aa5a361fac73550e62e0d82b65fad859a194bc5
elizad <elizadascal@gmail.com>
2020-01-14 02:44:38 +0000

    refactoring bids: btn tests moved

--- client/src/__tests__/button/__test__/button.test.js
+++ client/src/__tests__/button/__test__/button.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button from "./../button";
+import Button from "../button";
 
 import { render, cleanup } from "@testing-library/react";
 import "jest-dom/extend-expect";
